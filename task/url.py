adca09fe4f1fab6b8358d24403d5f19f574c6641
ibibeklamichhane <viveklc99@gmail.com>
2020-10-19 16:53:04 +0545

    remaining user update and initial delete

--- task/url.py
+++ task/url.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.index),
     path('update_task/<str:pk>/', views.UpdateTask,name='update_task'),
--- task/views.py
+++ task/views.py
@@ -12,7 +12,7 @@ def index(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect('/')
+            return redirect('/')
 
 
     context = {'task':task,'form':form}
@@ -24,7 +24,25 @@ def UpdateTask(request,pk):
     tasks=Task.objects.get(id=pk)
 
     form = TaskForm(instance=tasks)
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=tasks)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
     context= {'form':form}
 
     return render(request,'task/update_task.html',context)
+
+def DeleteTask(request,pk):
+
+    item=Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+
+        context={'item':item}
+        return render(request,'task/delete.html',context)
+
+
+
